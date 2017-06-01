9f7baf0c14bf1b42335dded0fa4c5e9d25b86bb3
Nanrou <kkkcomkkk@qq.com>
2017-06-01 12:10:41 +0800

    update property

--- coc/pages/bulid_card/bulid_card.js
+++ coc/pages/bulid_card/bulid_card.js
@@ -1,28 +1,48 @@
 // pages/bulid_card/bulid_card.js
+var s = 1;
+var that = this;
+
+
 
 Page({
-  
+
 // 把三个随机数函数放到工具那里去
   data: {
-    action: '',
-    result: 'none',
+    action: 'none',
+    result: '',
     point1: 1,
     point2: 1,
     point3: 1,
-    sum: 0,
-
+    
+    property: [
+      { name: '力量(3D6):', sum: 0, _type: 'str', status: 'roll' },
+      { name: '体质(3D6):', sum: 0, _type: 'con', status: 'roll' }, 
+      { name: '体型(3D6):', sum: 0, _type: 'siz', status: 'roll' },
+      { name: '敏捷(3D6):', sum: 0, _type: 'dex', status: 'roll' },
+      { name: '外貌(3D6):', sum: 0, _type: 'app', status: 'roll' },
+      { name: '意志(3D6):', sum: 0, _type: 'pow', status: 'roll' },
+      { name: '智力(2D6+6):', sum: 0, _type: 'int', status: 'roll' },
+      { name: '教育(2D6+6):', sum: 0, _type: 'edu', status: 'roll' },
+      { name: '幸运(3D6):', sum: 0, _type: 'luck', status: 'roll' },
+    ],
   },
 
+
   DiceAction: function (e) {
+    var index = e.currentTarget.dataset.id;
+    var pro = this.data.property[index];
+    pro.status === 'roll' ? 'stop' : 'roll';
+    console.log(pro);
+
     var a, r, p1, p2, p3;
-    var s = this.sum
+    var s = pro.sum;
     a = this.data.action === '' ? 'none' : '';
     r = this.data.result === 'none' ? '' : 'none';
 
-    p1 = this.rand1(6, 1);
-    p2 = this.rand2();
-    p3 = this.rand3(6);
     if (r === '') {
+      p1 = this.rand1(6, 1);
+      p2 = this.rand2();
+      p3 = this.rand3(6);
       s = p1 + p2 + p3;
     }
     this.setData({
@@ -31,11 +51,13 @@ Page({
       point1: p1,
       point2: p2,
       point3: p3,
-      sum: s,
-    });
-
+    },
+    
+    );
   },
 
+
+// product random
   rand1: function (max, min) {
     return Math.floor(Math.random() * (max - min + 1) + 1);
   },
